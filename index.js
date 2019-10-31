f3c26f0ced3626b0ea2ca818a327f7a45b45c861
Ali Naci Erdem <anacierdem@gmail.com>
2019-10-31 20:13:02 +0300

    Skip make install if no Makefile is found

--- index.js
+++ index.js
@@ -130,22 +130,30 @@ const availableActions = {
         if (paths.length > 1) {
           return Promise.reject('Using same dependency with different versions is not supported!');
         }
-        return new Promise(async (resolve, reject) => {
-          try {
-            const relativePath = path.relative(options.MOUNT_PATH, paths[0]).replace(new RegExp('\\' + path.sep), path.posix.sep);
-            const containerPath = path.posix.join('/', options.PROJECT_NAME, relativePath, '/');
-            const makePath = path.posix.join(containerPath, 'Makefile');
-
-            // Do not try to run docker if already in container
-            if (process.env.IS_DOCKER === 'true') {
-              await runCommand('[ -f ' + makePath + ' ] &&  make -C ' + containerPath + ' && make -C ' + containerPath + ' install');
-            } else {
-              await runCommand('docker exec ' + options.PROJECT_NAME + ' /bin/bash -c "[ -f ' + makePath + ' ] &&  make -C ' + containerPath + ' && make -C ' + containerPath + ' install"');
+        return new Promise((resolve, reject) => {
+          fs.access(path.join(paths[0], 'Makefile'), fs.F_OK, async (e) => {
+            if (e) {
+              // File does not exist - skip
+              resolve();
+              return;
             }
-            resolve();
-          } catch(e) {
-            reject(e);
-          }
+
+            try {
+              const relativePath = path.relative(options.MOUNT_PATH, paths[0]).replace(new RegExp('\\' + path.sep), path.posix.sep);
+              const containerPath = path.posix.join('/', options.PROJECT_NAME, relativePath, '/');
+              const makePath = path.posix.join(containerPath, 'Makefile');
+
+              // Do not try to run docker if already in container
+              if (process.env.IS_DOCKER === 'true') {
+                await runCommand('[ -f ' + makePath + ' ] &&  make -C ' + containerPath + ' && make -C ' + containerPath + ' install');
+              } else {
+                await runCommand('docker exec ' + options.PROJECT_NAME + ' /bin/bash -c "[ -f ' + makePath + ' ] &&  make -C ' + containerPath + ' && make -C ' + containerPath + ' install"');
+              }
+              resolve();
+            } catch(e) {
+              reject(e);
+            }
+          });
         });
       })
     );
